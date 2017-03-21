bbefb691fda1eda2ce27af3efacb05351561682d
Cretu Calin <cretucalin06@yahoo.ro>
2017-03-21 20:29:04 +0200

    Pojo

---
+++ Server/src/networking/Server.java
@@ -0,0 +1,47 @@
+package networking;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by calin on 21.03.2017.
+ */
+
+public class Server {
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+
+    private final int portNumber = 9797;
+
+    private int noOfPlayers;
+
+
+
+    public Server(){
+
+        try{
+            serverSocket = new ServerSocket(portNumber);
+            System.out.println("Server started");
+            noOfPlayers = 0;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void waitForConnections() {
+
+        while(true){
+
+            try{
+                clientSocket = serverSocket.accept();
+                System.out.println("Player connected");
+                noOfPlayers ++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
